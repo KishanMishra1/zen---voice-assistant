4356ade6949149e79bf3e90de2a16e227c1edf50
Kishan Mishra <kishanmishra@Kishans-MacBook-Air.local>
2022-05-05 08:11:31 +0530

    added

---
+++ ZENgui.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ZENgui.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 599)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.Zen_v1 = QtWidgets.QLabel(self.centralwidget)
+        self.Zen_v1.setGeometry(QtCore.QRect(0, -30, 851, 661))
+        self.Zen_v1.setText("")
+        self.Zen_v1.setPixmap(QtGui.QPixmap("main.gif"))
+        self.Zen_v1.setObjectName("Zen_v1")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(690, 410, 75, 23))
+        font = QtGui.QFont()
+        font.setFamily("Arial Black")
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet("background-color: rgb(0, 0, 0);\n"
+"border-color: rgb(255, 255, 255);\n"
+"color: rgb(255, 255, 255);")
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(690, 440, 75, 23))
+        font = QtGui.QFont()
+        font.setFamily("Arial Black")
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setStyleSheet("background-color: rgb(0, 0, 0);\n"
+"color: rgb(255, 255, 255);")
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(530, 20, 256, 41))
+        self.textBrowser.setStyleSheet("background:transparent;\n"
+"border-radius:none;\n"
+"color:white;\n"
+"font-size:20px;")
+        self.textBrowser.setObjectName("textBrowser")
+        self.textBrowser_2 = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser_2.setGeometry(QtCore.QRect(530, 60, 256, 41))
+        self.textBrowser_2.setStyleSheet("background:transparent;\n"
+"border-radius:none;\n"
+"color:white;\n"
+"font-size:20px;")
+        self.textBrowser_2.setObjectName("textBrowser_2")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(10, 10, 411, 91))
+        self.label_2.setText("")
+        self.label_2.setPixmap(QtGui.QPixmap("initiating.gif"))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(220, 120, 361, 361))
+        self.label_3.setText("")
+        self.label_3.setPixmap(QtGui.QPixmap("clc.gif"))
+        self.label_3.setScaledContents(True)
+        self.label_3.setObjectName("label_3")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(150, 522, 571, 51))
+        self.label.setText("")
+        self.label.setPixmap(QtGui.QPixmap("mic.gif"))
+        self.label.setObjectName("label")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(10, 550, 171, 41))
+        self.textEdit.setStyleSheet("\n"
+"border-color: rgb(0, 0, 0);\n"
+"color: rgb(255, 255, 255);\n"
+"background-color: transparent;")
+        self.textEdit.setObjectName("textEdit")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "START"))
+        self.pushButton_2.setText(_translate("MainWindow", "QUIT"))
+        self.textEdit.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Zen 1.0</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Creator: Kishan Mishra</span></p></body></html>"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
---
+++ Zen1_g/ZEN_uif.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ZEN_uif.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_ZEN(object):
+    def setupUi(self, ZEN):
+        ZEN.setObjectName("ZEN")
+        ZEN.resize(978, 600)
+        self.centralwidget = QtWidgets.QWidget(ZEN)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(0, 0, 981, 601))
+        self.label_2.setStyleSheet("")
+        self.label_2.setText("")
+        self.label_2.setPixmap(QtGui.QPixmap("../SciFi_LoaderBlue.gif"))
+        self.label_2.setScaledContents(True)
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(670, 460, 301, 131))
+        self.label_3.setText("")
+        self.label_3.setPixmap(QtGui.QPixmap("../initiating.gif"))
+        self.label_3.setScaledContents(True)
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(40, 170, 281, 281))
+        self.label_4.setText("")
+        self.label_4.setPixmap(QtGui.QPixmap("../1a7d825b332de7940e2a13e87cdd89c9.png"))
+        self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(760, 210, 201, 71))
+        self.label_5.setText("")
+        self.label_5.setPixmap(QtGui.QPixmap("../Credits.png"))
+        self.label_5.setScaledContents(True)
+        self.label_5.setObjectName("label_5")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(550, 20, 256, 61))
+        self.textBrowser.setStyleSheet("background: transparent;\n"
+"border-radius:none;\n"
+"color:white;\n"
+"font-size:20px;")
+        self.textBrowser.setObjectName("textBrowser")
+        self.textBrowser_2 = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser_2.setGeometry(QtCore.QRect(730, 20, 256, 61))
+        self.textBrowser_2.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.textBrowser_2.setStyleSheet("background: transparent;\n"
+"border-radius:none;\n"
+"color:white;\n"
+"font-size:20px;")
+        self.textBrowser_2.setObjectName("textBrowser_2")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(870, 340, 101, 41))
+        font = QtGui.QFont()
+        font.setFamily("Arial Black")
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton.setFont(font)
+        self.pushButton.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.pushButton.setStyleSheet("background-color: rgb(0, 0, 0);\n"
+"color: rgb(0, 170, 0);")
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(840, 380, 121, 41))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(14)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setStyleSheet("color: rgb(255, 85, 0);\n"
+"background-color: rgb(0, 0, 0);")
+        self.pushButton_2.setObjectName("pushButton_2")
+        ZEN.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(ZEN)
+        QtCore.QMetaObject.connectSlotsByName(ZEN)
+
+    def retranslateUi(self, ZEN):
+        _translate = QtCore.QCoreApplication.translate
+        ZEN.setWindowTitle(_translate("ZEN", "MainWindow"))
+        self.pushButton.setText(_translate("ZEN", "RUN"))
+        self.pushButton_2.setText(_translate("ZEN", "TERMINATE"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    ZEN = QtWidgets.QMainWindow()
+    ui = Ui_ZEN()
+    ui.setupUi(ZEN)
+    ZEN.show()
+    sys.exit(app.exec_())
+
